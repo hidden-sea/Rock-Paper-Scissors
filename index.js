7ff9919ce5fe1a17b05312d679829854c266a16b
aeleby <ae.mae.aw@gmail.com>
2022-12-30 09:24:39 -0800

    Edits to make the the rock paper scissors button register

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 "use strict";
-const choices = ["rock", "paper", "scissor"];
+const choices = ["rock", "paper", "scissors"];
 let computerScore;
 let playerScore;
-//define variablies in gloabal scop if they are listed in more than one function
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+//remember to place in hashtags in the parenthesis
+
+//define variablies in global scope if they are listed in more than one function
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
@@ -10,40 +16,70 @@ function getComputerChoice() {
 }
 //calling a function, use extra parenthsis so it will return value and not print itself
 //console.log(getComputerChoice());
-
-function getPlayerChoice() {
+/*function getPlayerChoice() {
   return prompt("What will you choose?").toLowerCase();
-}
+}*/
+
+rock.addEventListener("click", function () {
+  //2 functions within a function
+  playRound("rock", getComputerChoice());
+});
+
+paper.addEventListener("click", function () {
+  playRound("paper", getComputerChoice());
+});
+
+scissors.addEventListener("click", function () {
+  playRound("scissors", getComputerChoice());
+});
+
+const containers = document.querySelector(".containers"); //class have periods and ids have hashtags
+
+const content = document.createElement("div");
+content.classList.add("content");
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "scissor") {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
-    console.log("player wins; rock beats scissor");
+    content.textContent = "player wins; rock beats scissor";
+    containers.appendChild(content);
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
-    console.log("computer wins; paper beats rock");
-  } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    content.textContent = "computer wins; paper beats rock";
+    containers.appendChild(content);
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
-    console.log("computer wins; rock beats scissor");
-  } else if (playerSelection === "scissor" && computerSelection === "paper") {
+    content.textContent = "computer wins; rock beats scissor";
+    containers.appendChild(content);
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
-    console.log("player wins.scissor beats paper");
+    content.textContent = "player wins.scissor beats paper";
+    containers.appendChild(content);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
-    console.log("player wins. paper beats rock");
-  } else if (playerSelection === "paper" && computerSelection === "scissor") {
+    content.textContent = "player wins. paper beats rock";
+    containers.appendChild(content);
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
-    console.log("computer wins. scissor beats paper");
+    content.textContent = "computer wins. scissor beats paper";
+    containers.appendChild(content);
   } else {
-    console.log("its a draw");
+    content.textContent = "its a draw";
+    containers.appendChild(content);
   }
 }
+
+/*const playerSelection = ["rock", "paper", "scissors"];
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));*/
+const scores = document.createElement("div");
+scores.classList.add("scores");
 
 function game() {
   playerScore = 0;
   computerScore = 0;
-  for (let i = 1; i <= 5; i++) {
-    const playerSelection = getPlayerChoice();
+  for (let i = 0; i <= 5; i++) {
+    const playerSelection = ["rock", "paper", "scissors"];
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
     console.log(playerScore, computerScore);
@@ -52,9 +88,10 @@ function game() {
 //make sure to define the two parameters in playround() or the parameters will default to undefined
 console.log(game(5));
 if (playerScore < computerScore) {
-  console.log("computer is the winner");
-  console.log(computerScore);
+  scores.textContent = "computer is the winner";
+  scores.textContent = computerScore;
 } else if (computerScore < playerScore) {
-  console.log("player is the winner");
-  console.log(playerScore);
-} else console.log("everyone's a winner");
+  scores.textContent = "player is the winner";
+  scores.textContent = playerScore;
+} else scores.textContent = "everyone's a winner"; //make sure to delete parenthesis so js know its not a function
+containers.appendChild(scores);
